c87684092d75f2edc398fa0dfceeeb6a215f783b
grzegorz1q <grzegorbaryla@gmail.com>
2025-03-12 23:03:32 +0100

    Implemented GetAllMovies endpoint

---
+++ Server/MovieRental.Application/Interfaces/IMovieService.cs
@@ -0,0 +1,14 @@
+﻿using MovieRental.Application.Dtos.Movie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRental.Application.Interfaces
+{
+    public interface IMovieService
+    {
+        Task<IEnumerable<ReadMovieDto>> GetAllMovies();
+    }
+}
